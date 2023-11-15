12b86a859813af98bb6bb24ddb0886ec7a9229de
zahramutalib <zahramuta4@gmail.com>
2023-11-14 19:59:58 -0800

    This should work now please

--- exec_prompt.c
+++ exec_prompt.c
@@ -4,6 +4,7 @@
  * exec_prompt - a function for executing command prompt
  * @command:sort of a pointer
  */
+
 void exec_prompt(const char *command)
 {
 	pid_t child_pid = fork();
@@ -13,18 +14,17 @@ void exec_prompt(const char *command)
 		perror("fork");
 		exit(EXIT_FAILURE);
 	}
-
 	else if (child_pid == 0)
-	 {
-		 char *args[2];
-		 args[0] = (char *)command;
-		 args[1] = NULL;
+	{
+		char *args[2];
+
+		args[0] = (char *)command;
+		args[1] = NULL;
 
 		execve(command, args, (char *const *)NULL);
 		perror("execve");
 		exit(EXIT_FAILURE);
 	}
-
 	else
 	{
 		wait(NULL);
